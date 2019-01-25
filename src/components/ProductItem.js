74af79afbe82c836c8ad1e21f57674e43fae81c7
RFJC21 <rfj.cabeca@gmail.com>
2019-01-25 23:46:54 +0000

    first

---
+++ src/components/ProductItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class ProductItem extends Component {
+  render() {
+    return (
+      <div>
+        <h3>ProductItem</h3>
+      </div>
+    );
+  }
+}
