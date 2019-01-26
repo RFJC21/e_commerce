97189edea97c084ffabfc6402a04303021605806
RFJC21 <rfj.cabeca@gmail.com>
2019-01-26 23:49:38 +0000

    26jan

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 import logo from "../logo.svg";
 
@@ -18,12 +19,29 @@ export default class Navbar extends Component {
           </li>
         </ul>
         <Link to="/cart" className="ml-auto">
-          <button>
+          <ButtonContainer>
             <i className="fas fa-cart-plus" />
             My cart
-          </button>
+          </ButtonContainer>
         </Link>
       </nav>
     );
   }
 }
+
+//styling using styled components
+const ButtonContainer = styled.button`
+  text-transform: capitalize;
+  color: white;
+  font-size: 1.4rem;
+  background: transparent;
+  border: 0.05rem solid var(--lightBlue);
+  cursor: pointer;
+  padding: 0.2rem 0.5rem;
+  border-radius: 0.5rem;
+  margin: 0.2rem 0.5rem 0.2rem 0;
+  transition: all 0.05s ease-in-out;
+  &:hover {
+    background-color: var(--lightBlue);
+  }
+`;
